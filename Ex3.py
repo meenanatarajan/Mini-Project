79aef3d7314ad2a40bd18b637ce4d552c2c0ecf6
unknown <noorkhah.farshid@gmail.com>
2021-08-17 12:34:34 -0600

    updated the code by adding a pattern variable

--- Ex3.py
+++ Ex3.py
@@ -1,7 +1,8 @@
 import re
 def text_match(text):
         #Add your code here
-
+	patterns = 'ab{2,3}'
+	if re.search(patterns, text):
         
                 return 'Found a match!'
         else:
